b80526bdbb127beb9a7e1a564583d235ca8ef4c5
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-10-27 10:57:04 +0200

    adds changes

--- js/user.js
+++ js/user.js
@@ -1,6 +1,5 @@
 const userFullName = prompt("Inserisci il tuo nome intero ");
 const userColor = prompt("Inserisci il tuo colore preferito ");
-const userNumber = prompt("Inserisci numero 21 ");
 
 
 const h2NomeUtente = document.getElementById("nome_utente");
@@ -11,11 +10,7 @@ const h2ColoreUtente = document.getElementById("colore_utente");
 
 h2ColoreUtente.innerHTML = userColor;
 
-const h2NumberUtente = document.getElementById("number_utente");
-
-h2NumberUtente.innerHTML = userNumber;
-
 const smallcontainer = document.getElementById("small_container");
 
-small_container.innerHTML = `${userFullName}  ${userColor}  ${userNumber}`
+small_container.innerHTML = `${userFullName}${userColor}${userNumber}21`;
 
